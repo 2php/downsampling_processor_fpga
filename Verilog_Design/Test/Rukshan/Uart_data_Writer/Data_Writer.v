e5c1a6d6c1a22a533cbcb12bd046919686fc4f9c
Rukshan <Rukshan@github.com>
2018-03-21 23:26:35 +0530

    skips 15 garbage values

--- Verilog_Design/Test/Rukshan/Uart_data_Writer/Data_Writer.v
+++ Verilog_Design/Test/Rukshan/Uart_data_Writer/Data_Writer.v
@@ -7,27 +7,18 @@ output reg [7:0] Dout;
 output reg Wen=1'b0;
 output reg fin=0; 
 
-reg [3:0] counter=0;
-reg [1:0] STATE=2'b11;
+reg flag=0;
+reg [1:0] STATE=2'b0;
 
 parameter IDLE=2'b0;
 parameter STORING=2'b01;
 parameter DONE=2'b10;
-parameter PASS=2'b11;
+
 
   
 always @(posedge clk)
 begin
 	case(STATE)
-		PASS:
-			if (Rx_tick==1) 
-				begin
-				counter<=counter+1;
-		   		if (counter>=15)
-					begin   
-					STATE<=IDLE;
-					end
-				end
 		IDLE:
 			if(Rx_tick==1)
 				begin
@@ -36,8 +27,14 @@ begin
 				Dout<=Din;
 				STATE<=STORING;
 				end
-		STORING:
-			if(Addr==16'd65535) 
+		STORING: 
+			if ((Addr==16'd14) & flag==0)
+				begin  
+				flag<=1;
+				Addr<=16'b0;
+				STATE<=IDLE;
+				end
+			else if (Addr==16'd65535) 
 				begin
 				STATE<=DONE;
 				end
